9a90f9dcc280747b79929f56ff06fe279f57a9d8
Jhon Inofuente <jhonjb90@gmail.com>
2024-11-12 23:01:35 -0500

    iniciando proyecto

---
+++ src/main/java/pe/dhexsoft/retrofiRediss/RetrofiRedissApplication.java
@@ -0,0 +1,13 @@
+package pe.dhexsoft.retrofiRediss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrofiRedissApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrofiRedissApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/dhexsoft/retrofiRediss/RetrofiRedissApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.dhexsoft.retrofiRediss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrofiRedissApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
