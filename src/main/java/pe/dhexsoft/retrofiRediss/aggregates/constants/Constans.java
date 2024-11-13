68a3e90b9a8c4af024b93b17459286f6ec7c5932
Jhon Inofuente <jhonjb90@gmail.com>
2024-11-12 23:58:51 -0500

    VAMOS A CREAR NUESTRO REPONSE Y CONSTANTS

---
+++ src/main/java/pe/dhexsoft/retrofiRediss/aggregates/constants/Constans.java
@@ -0,0 +1,8 @@
+package pe.dhexsoft.retrofiRediss.aggregates.constants;
+
+public class Constans {
+    public final static String BEARER = "Bearer ";
+    public final static String BASE_URL = "https://api.apis.net.pe";
+    public final static int REDIS_TTL = 5;
+    public final static String REDIS_KEY_API_SUNAT = "MS:RETROFIT:JINOFUENTE";
+}
