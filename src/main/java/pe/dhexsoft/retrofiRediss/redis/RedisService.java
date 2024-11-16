91b2b92e37e5ebf8c0973a13a96235888b080e59
Jhon Inofuente <jhonjb90@gmail.com>
2024-11-16 09:19:35 -0500

    nuestro service para guardar y traer desde redis

--- src/main/java/pe/dhexsoft/retrofiRediss/redis/RedisService.java
+++ src/main/java/pe/dhexsoft/retrofiRediss/redis/RedisService.java
@@ -1,4 +1,28 @@
 package pe.dhexsoft.retrofiRediss.redis;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+//@RequiredArgsConstructor --> esto tambien te permite la inyeccion de constructor
 public class RedisService {
+
+     private final StringRedisTemplate stringRedisTemplate;
+
+    public RedisService(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    public void saveInRedis(String key, String value, int expiration){
+        stringRedisTemplate.opsForValue().set(key, value);
+        stringRedisTemplate.expire(key,expiration, TimeUnit.MINUTES);
+    }
+
+    public String getDataFromRedis(String key){
+        return stringRedisTemplate.opsForValue().get(key);
+    }
 }
--- src/main/java/pe/dhexsoft/retrofiRediss/util/Util.java
+++ src/main/java/pe/dhexsoft/retrofiRediss/util/Util.java
@@ -5,13 +5,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Util {
 
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper(); //el OBJECT MAPPER se utiliza para convertir objetos jaca a represetnaciones json y viciversa
 
+    //aqui con ewl object mapper vamos a convertir a string
     public static <T> String convertirAString(T object)
         throws JsonProcessingException {
-        return OBJECT_MAPPER.writeValueAsString(object);
+        return OBJECT_MAPPER.writeValueAsString(object); //
     }
 
+    //aqui vamos convertir de un string a un objeto
     public static <T> T convertirDesdeString(String datoDeRedis, Class<T> tipoClase)
         throws JsonProcessingException{
         return OBJECT_MAPPER.readValue(datoDeRedis, tipoClase);
