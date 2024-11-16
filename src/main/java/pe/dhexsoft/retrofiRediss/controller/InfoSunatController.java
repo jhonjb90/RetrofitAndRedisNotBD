6ab0578d01fede0caf8f116be98d690fd8d705db
Jhon Inofuente <jhonjb90@gmail.com>
2024-11-16 18:08:35 -0500

    agregando nuestro io y la info de redis

--- src/main/java/pe/dhexsoft/retrofiRediss/controller/InfoSunatController.java
+++ src/main/java/pe/dhexsoft/retrofiRediss/controller/InfoSunatController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import pe.dhexsoft.retrofiRediss.aggregates.response.ResponseSunat;
 import pe.dhexsoft.retrofiRediss.service.InfoSunatService;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/v1/retrofit/")
 @RequiredArgsConstructor
@@ -18,7 +20,7 @@ public class InfoSunatController {
     private final InfoSunatService infoSunatService;
 
     @GetMapping("/infoSunat")
-    public ResponseEntity<ResponseSunat> getIfnroSunat(@RequestParam("ruc") String ruc){
+    public ResponseEntity<ResponseSunat> getIfnroSunat(@RequestParam("ruc") String ruc) throws IOException {
         return  new ResponseEntity<>(infoSunatService.getInfoSunat(ruc), HttpStatus.OK);
     }
 }
