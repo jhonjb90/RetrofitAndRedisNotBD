475197615959c30c2d74efcbffc1bf029a75d27f
Jhon Inofuente <jhonjb90@gmail.com>
2024-11-16 18:04:07 -0500

    ya hemos construido nuestro controller con get

---
+++ src/main/java/pe/dhexsoft/retrofiRediss/controller/InfoSunatController.java
@@ -0,0 +1,24 @@
+package pe.dhexsoft.retrofiRediss.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pe.dhexsoft.retrofiRediss.aggregates.response.ResponseSunat;
+import pe.dhexsoft.retrofiRediss.service.InfoSunatService;
+
+@RestController
+@RequestMapping("/v1/retrofit/")
+@RequiredArgsConstructor
+public class InfoSunatController {
+
+    private final InfoSunatService infoSunatService;
+
+    @GetMapping("/infoSunat")
+    public ResponseEntity<ResponseSunat> getIfnroSunat(@RequestParam("ruc") String ruc){
+        return  new ResponseEntity<>(infoSunatService.getInfoSunat(ruc), HttpStatus.OK);
+    }
+}
